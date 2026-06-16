7e2006bd96a1ff32fea88131982c0fd68b2c327b
Ben Vanheerswynghels <ben.vanheerswynghels@student.ehb.be>
2026-06-16 13:06:53 +0200

    Co-authored-by: LennertDeGols <LennertDeGols@users.noreply.github.com> Co-authored-by: BorisMeeusen <BorisMeeusen@users.noreply.github.com>  filter finished

---
+++ js/home.js
@@ -0,0 +1,39 @@
+
+const locations = document.getElementById("location");
+const filter_confirm = document.getElementById("filter_confirm");
+const suggestion1 = document.getElementById("suggestion1");
+const suggestion2 = document.getElementById("suggestion2");
+const suggestion3 = document.getElementById("suggestion3");
+
+
+
+
+
+// const params = new URLSearchParams(window.location.search);
+
+// const locatieParam = params.get("locatie");
+// const jaarParam = params.get("jaar");
+
+// if (locatieParam) {
+
+//     location_box.innerText = locatieParam;
+
+//     setTimeout(() => {
+//         filter_confirm.click();
+//     }, 100);
+
+// }
+
+// if (jaarParam) {
+
+//     date_lowest.value = jaarParam;
+//     date_highest.value = jaarParam;
+
+//     setTimeout(() => {
+//         filter_confirm.click();
+//     }, 100);
+
+// }
+
+
+/*createFilteredImages(images);*/
--- js/index.js
+++ js/index.js
@@ -19,6 +19,18 @@ const location_3 = document.getElementById("location_3");
 const location_3_button = document.getElementById("location_3_button");
 const location_geen = document.getElementById("location_geen");
 const location_geen_button = document.getElementById("location_geen_button");
+const imageList = document.getElementById("image_list");
+const input = document.getElementById("searchbar");
+const overlay_image = document.getElementById("overlay_image");
+const title = document.getElementById("title");
+const date = document.getElementById("date");
+const chosen_location = document.getElementById("dropdown-grijs");
+const locations = document.getElementById("location");
+const filter_confirm = document.getElementById("filter_confirm");
+const date_lowest = document.getElementById("date_lowest");
+const date_highest = document.getElementById("date_highest");
+const check1 = document.getElementById("check1");
+const check3 = document.getElementById("check3");
 
 (async () => {
     const imagesRes = await fetch("../js/beeldbank.json");
@@ -84,27 +96,32 @@ const location_geen_button = document.getElementById("location_geen_button");
     });
 })();
 
-function createFilteredImages(images) {
-    const imageList = document.getElementById("image_list");
-    const input = document.getElementById("searchbar");
-    const overlay_image = document.getElementById("overlay_image");
-    const title = document.getElementById("title");
-    const date = document.getElementById("date");
-    const location = document.getElementById("location");
-    let filename_date = "Geen datum";
 
+
+function createFilteredImages(images) {
     imageList.innerHTML = "";
 
     images.forEach(imageData => {
+        let filename_date = "Geen datum";
+        let filename_location = "Geen locatie";
         const filename = imageData.filename.slice(0, -4);
         const filename_list = filename.split(/[ ,-]+/);
         filename_list.forEach(filename_part => {
             const year = Number(filename_part);
             if (year > 1000 && year < 5000) {
                 filename_date = filename_part;
+                filename_date = Number(filename_date);
+            }
+            let filename_part_lower = filename_part.toLowerCase();
+            if (filename_part_lower == "schaarbeek") {
+                filename_location = "Schaarbeek";
+            } else if (filename_part_lower == "gillis") {
+                filename_location = "St-Gillis";
+            } else if (filename_part_lower == "noordwijk") {
+                filename_location = "Noordwijk";
             }
         });
-        
+
         const container = document.createElement("li");
         container.className = "filtered_image_container";
 
@@ -122,13 +139,6 @@ function createFilteredImages(images) {
         container.appendChild(text);
         imageList.appendChild(container);
 
-        image.addEventListener("click", () => {
-            overlay.style.display = "block";
-            overlay_image.src = imageData.path;
-            title.innerText = filename;
-            date.innerText = filename_date;
-        });
-
         input.addEventListener("input", () => {
             const lowName = filename.toLowerCase();
             const lowInput = input.value.toLowerCase();
@@ -138,5 +148,72 @@ function createFilteredImages(images) {
                 container.setAttribute("hidden", true);
             }
         });
+
+        filter_confirm.addEventListener("click", () => {
+            container.setAttribute("hidden", true);
+            if (date_lowest.value != "" || date_highest.value != "") {
+                if (filename_date != "Geen datum") {
+                    if (date_lowest.value == "" && date_highest.value != "") {
+                        if (filename_date <= Number(date_highest.value)) {
+                            filter_part();
+                        } else {
+                            container.setAttribute("hidden", true);
+                        }
+                    } else if (date_lowest.value != "" && date_highest.value == "") {
+                        if (filename_date >= Number(date_lowest.value)) {
+                            filter_part();
+                        } else {
+                            container.setAttribute("hidden", true);
+                        }
+                    } else if (date_lowest.value != "" && date_highest.value != "") {
+                        if (filename_date >= Number(date_lowest.value) && filename_date <= Number(date_highest.value)) {
+                            filter_part();
+                        } else {
+                            container.setAttribute("hidden", true);
+                        }
+                    }
+                } else {
+                    container.setAttribute("hidden", true);
+                }
+            } else if (chosen_location.innerText.toLowerCase() != "-") {
+                if (filename.toLowerCase().includes(chosen_location.innerText.toLowerCase())) {
+                    container.removeAttribute("hidden");
+                } else {
+                    container.setAttribute("hidden", true);
+                }
+            } else {
+                container.removeAttribute("hidden");
+            }
+            if (check1.checked) {
+                if (filename_date == "Geen datum" || filename_location == "Geen locatie") {
+                    container.removeAttribute("hidden");
+                }
+            }
+            if (check3.checked) {
+                if (!filename.toLowerCase().includes("kaart")) {
+                    container.setAttribute("hidden", true);
+                }
+            }
+        });
+
+        image.addEventListener("click", () => {
+            overlay.style.display = "block";
+            overlay_image.src = imageData.path;
+            title.innerText = filename;
+            date.innerText = filename_date;
+            locations.innerText = filename_location;
+        });
+
+        function filter_part() {
+            if (chosen_location.innerText.toLowerCase() != "-") {
+                if (filename.toLowerCase().includes(chosen_location.innerText.toLowerCase())) {
+                    container.removeAttribute("hidden");
+                } else {
+                    container.setAttribute("hidden", true);
+                }
+            } else {
+                container.removeAttribute("hidden");
+            }
+        }
     });
 }
